0ecd569b28314a663489b79d4ccb153e64d0a93c
athul-krishnan-b <bkrishathul@gmail.com>
2022-07-08 12:19:38 +0530

    complete

--- src/app/viewstudents/viewstudents.component.ts
+++ src/app/viewstudents/viewstudents.component.ts
@@ -10,6 +10,16 @@ export class ViewstudentsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  }
+ 
+ }
+data=
+[
+{"name":"Rahul","age":24,"photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyK6VC1UacbsCU2hKSWCItKSrh1hRq35XDKg&usqp=CAU"},
+{"name":"Tharun","age":15,"photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyK6VC1UacbsCU2hKSWCItKSrh1hRq35XDKg&usqp=CAU"},
+{"name":"Amal","age":19,"photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyK6VC1UacbsCU2hKSWCItKSrh1hRq35XDKg&usqp=CAU"},
+{"name":"Athul","age":20,"photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyK6VC1UacbsCU2hKSWCItKSrh1hRq35XDKg&usqp=CAU"}
 
+
+
+]
 }
